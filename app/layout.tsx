835693f9e7189daa9dbf90b2a7688d87854fbaa5
Aaron Torres <aaron.torres.dev@gmail.com>
2024-03-12 11:01:20 -0300

    Fix formatting and refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./navbar";
+import Footer from "./footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,10 +18,15 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>
-                <div className="px-8 lg:px-32 md:px-16">
+            <body
+                className={inter.className}
+                style={{
+                    backgroundColor: "#000000",
+                    backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='28' height='49' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='hexagons' fill='%234984ff' fill-opacity='0.12' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");`,
+                }}>
+                <div>
                     <Navbar />
-                    <div className="py-20">{children}</div>
+                    <div className="p-8 mx-8 mt-20 mb-8 md:mx-32">{children}</div>
                 </div>
             </body>
         </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
+import SocialMediaButtons from "./components/SocialMediaButtons";
 
 export default function Home() {
     return (
-        <div className="grid grid-cols-1 gap-14 md:grid-cols-2 justify-items-center">
-            <div>
-                <h1 className="p-8 text-4xl font-bold text-blue-300 font-inter">{`Hi, I'm Aaron Torres, a Software Engineer.`}</h1>
+        <div className="grid grid-cols-1 gap-14 md:grid-cols-2 justify-items-center place-items-center">
+            <div className="grid justify-items-center place-items-center">
+                <h1 className="text-4xl font-bold text-blue-300 font-inter">{`Hi, I'm Aaron Torres, a Software Engineer.`}</h1>
+                <br />
+                <p>{`I'm a passionate Full Stack web developer with experience developing Full Stack web applications with Python, Django, React.js, and Cloud Technologies. I am strongly interested in learning new technologies and implementing them in my projects. I'm a self-motivated and hardworking individual who is always ready to learn new things and work in a team.`}</p>
+                <br />
+                <span className="text-lg text-white font-inter">{`Currently based in Raleigh, NC. Looking for a hybrid or fully remote Front-end, Back-end, or Full-stack Software Engineering position.`}</span>
+                <br />
+                <SocialMediaButtons />
             </div>
             <div>
                 <Image src="/computer.png" width={300} height={300} className="img-fluid" alt="computer" />
